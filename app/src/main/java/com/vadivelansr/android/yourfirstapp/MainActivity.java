287b5eadefcc13f570f351d12d16cad76581829c
vadivelansr <vadivelan.sr@gmail.com>
2016-06-09 07:25:19 +0530

    Initial Commit
    Initial commit

---
+++ app/src/main/java/com/vadivelansr/android/yourfirstapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.vadivelansr.android.yourfirstapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(getString(R.string.app_name));
+    }
+}
